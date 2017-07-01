f7452b9822bae8a68ff11a2eb72fb1375c7b186c
kamsac <kamsac@users.noreply.github.com>
2017-07-01 18:59:56 +0200

    Rename 'app' folder to 'src'.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        main: './app/index.ts'
+        main: './src/index.ts'
     },
     output: {
         filename: '[name].js',
